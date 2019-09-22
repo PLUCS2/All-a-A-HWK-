a080d15186fbb9628d926127d18cbbe63f5d2421
Paloma <psecunda@elon.edu>
2019-09-22 16:29:43 -0400

    Homework 1

---
+++ W7D1/frontend/actions.js
@@ -0,0 +1,13 @@
+
+
+let selectLocation = function(city, jobs) {
+    return {
+        type: "SWITCH_LOCATION", 
+        city: city, 
+        jobs: jobs
+    }
+}
+
+window.selectLocation = selectLocation; 
+
+export default selectLocation;
